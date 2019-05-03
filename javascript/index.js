e07d2fc208c40f40d2228c6337abea78ee9470e2
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-03 13:15:12 -0400

    completed hourglass problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,17 @@
 
 function hourglassSum(arr) {
-    let possibleIndexes = [0,1,2,3,6,7,8,9,12,13,14,15,18,19,20,21]
-
+  function reduceHourglass(i) {
+    return arr[i] + arr[i+1] + arr[i+2] + arr[i+7] + arr[i+12] + arr[i+13] + arr[i+14]
+  }
+  let possibleIndexes = [0,1,2,3,6,7,8,9,12,13,14,15,18,19,20,21]
+  let topHourglassIndex = possibleIndexes[0]
+  possibleIndexes.forEach(index => {
+    let hourglassValue = reduceHourglass(index)
+    if (reduceHourglass(topHourglassIndex) < hourglassValue) {
+      topHourglassIndex = index
+    }
+  })
+  return reduceHourglass(topHourglassIndex)
 }
 
 // -9 -9 -9  1 1 1
@@ -10,7 +20,7 @@ function hourglassSum(arr) {
 //  0  0  8  6 6 0
 //  0  0  0 -2 0 0
 //  0  0  1  2 4 0
-
+// console.log(hourglassSum([-9,-9,-9,1,1,1,0,-9,0,4,3,2,-9,-9,-9,1,2,3,0,0,8,6,6,0,0,0,0,-2,0,0,0,0,1,2,4,0]))
 // -63, -34, -9, 12,
 // -10, 0, 28, 23,
 // -27, -11, -2, 10,
@@ -21,11 +31,11 @@ function hourglassSum(arr) {
       // 8 6 6
 
 
-// 1 1 1 0 0 0
-// 0 1 0 0 0 0
-// 1 1 1 0 0 0
-// 0 0 2 4 4 0
-// 0 0 0 2 0 0
-// 0 0 1 2 4 0
+console.log(hourglassSum([1,1,1,0,0,0,
+0,1,0,0,0,0,
+1,1,1,0,0,0,
+0,0,2,4,4,0,
+0,0,0,2,0,0,
+0,0,1,2,4,0]))
 
 // outputs 19
