4d075a62b0dc1772ec7103f30619db6d7ffb134b
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-06 13:27:10 -0400

    wrote function to calc minbribeforline

--- javascript/index.js
+++ javascript/index.js
@@ -9,14 +9,37 @@ function minimumBribes(q){
   let t = q.shift()
 
   for (var i = 0; i < t; i++) {
-    minBribeForLine(q.slice(1,q[0]))
+    console.log(minBribeForLine(q.slice(1,q[0])))
     q.splice(0,q[0])
   }
 }
 
 function minBribeForLine(line) {
-
+  let bribes = 0
+  let tooChaotic = line.find((el, i) => {
+    let numBribes = el - (i + 1)
+    // console.log("********");
+    // console.log("ELEMENT: ", el);
+    // console.log("POSITION: ", i + 1);
+    // console.log("NUM BRIBES: ", numBribes);
+    // console.log("********");
+    if (numBribes > 2){
+      return true
+    } else {
+      if (numBribes > 0 || line[i+1] < el ){
+        bribes += Math.abs(numBribes)
+      }
+      return false
+    }
+  })
+  return tooChaotic ? "Too chaotic" : bribes
 }
+
+console.log(minBribeForLine([2,1,5,3,4])) // 3
+console.log(minBribeForLine([2,5,1,3,4])) // Too chaotic
+console.log(minBribeForLine([5, 1, 2, 3, 7, 8, 6, 4])) // Too chaotic
+console.log(minBribeForLine([1, 2, 5, 3, 7, 8, 6, 4])) // 7
+console.log(minBribeForLine([1, 2, 5, 3, 4, 7, 8, 6])) // 4
 
 // ex:
 //   [
