eefe61421bd937f9e7b883464bdc8d4c1969a486
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-06 12:54:00 -0400

    setup new year chaos problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,41 +1,22 @@
+// Print an integer denoting the minimum number of bribes needed to get the queue
+// into its final state. Print Too chaotic if the state is invalid,
+// i.e. it requires a person to have bribed more than people.
 
-function hourglassSum(arr) {
-  function reduceHourglass([y,x]) {
-    return arr[y][x] + arr[y][x+1] + arr[y][x+2] + arr[y+1][x+1] + arr[y+2][x] + arr[y+2][x+1] + arr[y+2][x+2]
-  }
-  let possibleIndexes = [[0,0],[0,1],[0,2],[0,3],[0,4],[1,0],[1,1],[1,2],[1,3],[1,4],[2,0],[2,1],[2,2],[2,3],[2,4],[3,0],[3,1],[3,2],[3,3],[3,4]]
-  let topHourglassIndex = possibleIndexes[0]
-  possibleIndexes.forEach(index => {
-    let hourglassValue = reduceHourglass(index)
-    if (reduceHourglass(topHourglassIndex) < hourglassValue) {
-      topHourglassIndex = index
-    }
-  })
-  return reduceHourglass(topHourglassIndex)
+function minimumBribes(q){
+  // q is an array of integers
+  // the first integer (t) describes the number of test cases in q
+  // t times, there will be an integer (n) followed by n-integers. n describes how many people are in line (n-integers is the line)
+
 }
 
-// -9 -9 -9  1 1 1
-//  0 -9  0  4 3 2
-// -9 -9 -9  1 2 3
-//  0  0  8  6 6 0
-//  0  0  0 -2 0 0
-//  0  0  1  2 4 0
-console.log(hourglassSum([[-9,-9,-9,1,1,1],[0,-9,0,4,3,2],[-9,-9,-9,1,2,3],[0,0,8,6,6,0],[0,0,0,-2,0,0],[0,0,1,2,4,0]]))
-// -63, -34, -9, 12,
-// -10, 0, 28, 23,
-// -27, -11, -2, 10,
-// 9, 17, 25, 18
-
-// 28 => 0 4 3
-      //   1
-      // 8 6 6
-
-
-// console.log(hourglassSum([1,1,1,0,0,0,
-// 0,1,0,0,0,0,
-// 1,1,1,0,0,0,
-// 0,0,2,4,4,0,
-// 0,0,0,2,0,0,
-// 0,0,1,2,4,0]))
-
-// outputs 19
+// ex: 
+//   [
+//     2,
+//     5,
+//     2, 1, 5, 3, 4,
+//     5,
+//     2, 5, 1, 3, 4,
+//   ]
+// would output:
+// 3
+// Too chaotic
