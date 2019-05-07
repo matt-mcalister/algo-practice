8c20253663d1fe69805aa84f45bcf2107db9d62a
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-07 16:30:50 -0400

    solved song decoder

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,7 @@
 function songDecoder(song){
-  // ...
+  return song.replace(/^(WUB){1,}|(WUB){1,}$/g, "").replace(/(WUB){1,}/g, " ")
 }
 
 // remove the WUBS
-// songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB")
+console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"))
   // =>  WE ARE THE CHAMPIONS MY FRIEND
