27d2172b9cc9950d80dd01481dc26a27c89457f0
matt-mcalister <matt.mcalister93@gmail.com>
2019-05-08 12:55:38 -0400

    ordered weight problemset

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,6 @@
-function songDecoder(song){
-  return song.replace(/^(WUB){1,}|(WUB){1,}$/g, "").replace(/(WUB){1,}/g, " ")
+function orderWeight(strng) {
+    // your code
 }
 
-// remove the WUBS
-console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"))
-  // =>  WE ARE THE CHAMPIONS MY FRIEND
+// "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes:
+  // "100 180 90 56 65 74 68 86 99"
