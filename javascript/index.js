1ccc01ad0c3a4ac39fe75c9f637592987cdbf920
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-16 16:49:14 -0400

    setup isValid function for sherlock perfect string problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,7 @@
-function alternatingCharacters(str){
-  let result = str[0]
-  for (var i = 1; i < str.length; i++) {
-    result = result[result.length-1] != str[i] ? result + str[i] : result
-  }
-  return str.length - result.length
+function isValid(str){
+
 }
 
-console.log(alternatingCharacters("AAAA"))
-console.log(alternatingCharacters("BBBBB"))
-console.log(alternatingCharacters("ABABABAB"))
-console.log(alternatingCharacters("BABABA"))
-console.log(alternatingCharacters("AAABBB"))
+console.log(isValid("aabbcd"))
+console.log(isValid("aabbccddeefghi"))
+console.log(isValid("abcdefghhgfedecba"))
