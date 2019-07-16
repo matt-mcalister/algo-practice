5b80a3602590b5c1241abdd5c67d245fa10ca016
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-16 14:10:06 -0400

    found solution of log(N)

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,22 @@
+function makeDictionary(acc, el){
+  acc[el] = acc[el] || 0
+  acc[el]++
+  return acc
+}
 
-function makeAnagram(a, b) {
-  return 0
+function makeAnagram(stringA, stringB) {
+  let dictA = stringA.split("").reduce(makeDictionary, {})
+  let dictB = stringB.split("").reduce(makeDictionary, {})
+  function compare(acc, el){
+    let compA = {...dictA}
+    let compB = {...dictB}
+    compA[el] = compA[el] || 0
+    compB[el] = compB[el] || 0
+    return acc + Math.abs(compA[el] - compB[el])
+  }
+  return Object.keys({...dictA, ...dictB}).reduce(compare, 0)
 }
 
 console.log(makeAnagram("cde","abc") === 4)
+console.log(makeAnagram("fcrxzwscanmligyxyvym","jxwtrhvujlmrpdoqbisbwhmgpmeoke") === 30)
+console.log(makeAnagram("showman","woman") === 2)
