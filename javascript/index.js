ff39842fb5bc929f2382d52a89590ea6f2d37518
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-16 16:25:15 -0400

    found o(n) solution to uniqChars

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,14 @@
 // Given string and int size, find if the string is unique up to the length specified in size.
 function uniqChars(str){
-  
+  let dict = {}
+  for (var i = 0; i < str.length; i++) {
+    dict[str[i]] = dict[str[i]] || 0
+    dict[str[i]]++
+    if (dict[str[i]] > 1){
+      return false
+    }
+  }
+  return true
 }
 
 
