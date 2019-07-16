c8f650f86cb7eb0ba3c6ac6f58141dba88bb3d9c
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-16 17:01:02 -0400

    found O(n) solution to sherlock string problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,22 @@
 function isValid(str){
-
+  let dict = str.split("").reduce((acc,el) => {
+    acc[el] = acc[el] || 0
+    acc[el]++
+    return acc
+  }, {})
+  let removed = false
+  let letters = Object.keys(dict)
+  let num = dict[letters[0]]
+  for (el of letters) {
+    if (dict[el] != num) {
+      if (removed){
+        return "NO"
+      } else {
+        removed = true
+      }
+    }
+  }
+  return "YES"
 }
 
 console.log(isValid("aabbcd"))
