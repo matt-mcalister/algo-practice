c0283c5aa6ce6d3c972aef0873f5601043933a57
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-17 14:22:49 -0400

    setup special substring problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,24 +1,5 @@
-function isValid(str){
-  let dict = str.split("").reduce((acc,el) => {
-    acc[el] = acc[el] || 0
-    acc[el]++
-    return acc
-  }, {})
-  let removed = false
-  let letters = Object.keys(dict)
-  let num = dict[letters[0]]
-  for (el of letters) {
-    if (dict[el] != num) {
-      if (removed){
-        return "NO"
-      } else {
-        removed = true
-      }
-    }
-  }
-  return "YES"
+function substrCount(str){
+
 }
 
-console.log(isValid("aabbcd"))
-console.log(isValid("aabbccddeefghi"))
-console.log(isValid("abcdefghhgfedecba"))
+console.log(substrCount("asasd") === 7)
