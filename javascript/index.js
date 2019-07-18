cc83555bac892765d4f5a6fb52755c0c711ee0cc
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-18 17:19:01 -0400

    found O(n^2) solution

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,13 @@
 function maximumSubArray(arr){
-  
+  let max = arr[0]
+  let newSum = arr[0]
+  for (let leftBound = 0; leftBound < arr.length; leftBound++) {
+    for (let rightBound = leftBound+1; rightBound <= arr.length; rightBound++) {
+      newSum = arr.slice(leftBound, rightBound).reduce(((acc, el) => acc + el), 0)
+      max = newSum > max ? newSum : max
+    }
+  }
+  return max
 }
 
 console.log(maximumSubArray([-2,-3,4,-1,-2,1,5,-3]) === 7)
