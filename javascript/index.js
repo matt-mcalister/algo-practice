38ddc7237b9a09a27e7dd7463177b49349cc9391
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-20 12:57:49 -0400

    solution to maximumToys problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,21 @@
 function maximumToys(arr, budget){
-
+  let sorted = arr.sort((a,b) => {
+    if (a < b) {
+      return -1
+    } else if (a > b) {
+      return 1
+    } else {
+      return 0
+    }
+  })
+  
+  return sorted.reduce((acc, el) => {
+    if (el < budget) {
+      budget -= el
+      acc++
+    }
+    return acc
+  }, 0)
 }
 
 console.log(maximumToys([1,12,5,111,200,1000,10], 50) === 4)
