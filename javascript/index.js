2ba8f5930e6c657e7c605b18a8f08e2d77977231
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-20 12:26:52 -0400

    found solution to numSwaps bubblesort

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,22 @@
 function countSwaps(arr){
+  let sorted = [...arr]
   let numSwaps = 0
+
+  for (let i = 0; i < sorted.length; i++) {
+    for (let j = 0; j < sorted.length - 1; j++) {
+        if (sorted[j] > sorted[j + 1]) {
+          let bigger = sorted[j]
+          let smaller = sorted[j+1]
+          sorted[j] = smaller
+          sorted[j+1] = bigger
+          numSwaps++
+        }
+    }
+}
   console.log(`Array is sorted in ${numSwaps} swaps.`)
-  console.log(`First Element: ${arr[0]}`)
-  console.log(`Last Element: ${arr[arr.length - 1]}`)
+  console.log(`First Element: ${sorted[0]}`)
+  console.log(`Last Element: ${sorted[sorted.length - 1]}`)
+  return sorted
 }
 
 console.log(countSwaps([6,4,1])) // 3,1,6
