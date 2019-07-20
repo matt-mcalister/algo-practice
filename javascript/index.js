88ca14a0ab10707475eee3c23a519bbd1595d40a
matt-mcalister <matt.mcalister93@gmail.com>
2019-07-20 12:23:06 -0400

    setup numSwaps bubblesort problem

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,10 @@
-function maximumSubArray(arr){
-  let max = -Infinity
-
-  let currentSum = 0
-
-  arr.forEach((el, idx) => {
-    currentSum += el
-
-    if (currentSum > max) {
-      max = currentSum
-    }
-
-    if (currentSum < 0){
-      currentSum = 0
-    }
-
-  })
-  return max
+function countSwaps(arr){
+  let numSwaps = 0
+  console.log(`Array is sorted in ${numSwaps} swaps.`)
+  console.log(`First Element: ${arr[0]}`)
+  console.log(`Last Element: ${arr[arr.length - 1]}`)
 }
 
-console.log(maximumSubArray([-2,-3,4,-1,-2,1,5,-3]) === 7)
-console.log(maximumSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]) === 6)
+console.log(countSwaps([6,4,1])) // 3,1,6
+console.log(countSwaps([1,2,3])) // 0,1,3
+console.log(countSwaps([3,2,1])) // 3,1,3
